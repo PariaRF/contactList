4c2efcbe2d67d29f9603a083f95faf6c554ca163
paria rezaee fard <paria_rezaee4224@yahoo.com>
2020-11-26 23:19:50 +0330

    initial files

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h2>Contact List </h2>
+        {/* import and show Contact List Component in here */}
+      </header>
+    </div>
+  );
+}
+
+export default App;
